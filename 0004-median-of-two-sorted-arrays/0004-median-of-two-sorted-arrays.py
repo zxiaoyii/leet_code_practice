b26cfdc81cb17c4d638457703f61525c058d630f
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-10 14:45:13 -0800

    Time: 0 ms (100%), Space: 19.8 MB (10.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,26 +1,49 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # nums1 = [1,|2, 3]
+        # nums2 = [2, 3, 4, |5]
+        # len(nums1) = m = 3
+        # len(nums2) = n = 4
+        # m + n = 7
+        # [1, 2, 2, 3, | 3, 4, 5]
+        # a = (m + n + 1) // 2 = 4
+        #[ 1, 2, 2, 3] & [3, 4, 5 ]
+        
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
         m, n = len(nums1), len(nums2)
-        half = (m + n + 1) // 2
+        a = (m + n + 1) // 2
         l, r = 0, m
+        
         while l <= r:
-            i = l + (r - l) // 2
-            j = half - i
-            nums1_left = nums1[i - 1] if i > 0 else float('-inf')
-            nums1_right = nums1[i] if i < m else float('inf')
-            nums2_left = nums2[j - 1] if j > 0 else float('-inf')
-            nums2_right = nums2[j] if j < n else float('inf')
-            if nums1_left <= nums2_right and nums2_left <= nums1_right:
+            i = (l + r) // 2 
+            j = a - i
+            
+            nums1_a = nums1[i - 1] if i > 0 else float('-inf')
+            nums1_b = nums1[i] if i < m else float('inf')
+            nums2_a = nums2[j - 1] if j > 0 else float('-inf')
+            nums2_b = nums2[j] if j < n else float('inf')
+            # nums1 = [|1,2,3]
+            # nums2 = [2, 3, 4,5|]
+            if nums1_a <= nums2_b and nums1_b >= nums2_a:
                 if (m + n) % 2 == 1:
-                    return max(nums1_left, nums2_left)  # 奇数：左半边最大值
+                    return max(nums1_a, nums2_a)
                 else:
-                    return (max(nums1_left, nums2_left) +
-                            min(nums1_right, nums2_right)) / 2  # 偶数：取平均
-            elif nums1_left > nums2_right:
-                r = i - 1  # nums1 取多了，往左收
+                    return (max(nums1_a, nums2_a) + min(nums1_b, nums2_b)) / 2
+            elif nums1_a > nums2_b:
+                r = i - 1
             else:
-                l = i + 1   # nums1 取少了，往右扩
-
-        return 0  # 不会到这里
+                l = i + 1
+        # return 0
+                
+            
+                    
+                    
+                
+            
+            
+        
+        
+        
+        
+        
