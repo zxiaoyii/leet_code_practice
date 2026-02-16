ae4a82af60dd199b173ad2bb6ea00fe05a52449c
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-15 21:15:31 -0800

    Time: 13 ms (61.32%), Space: 28.6 MB (36.31%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,37 +1,63 @@
 class Twitter:
 
     def __init__(self):
-        self.time = 0
-        self.tweets = defaultdict(list) #userId -> [(time, tweetId), ...]
-        self.following = defaultdict(set) #userId -> set of followeeIds
+        self.following = defaultdict(set) # {followerId: {followeeId, ..}}
+        self.tweets = defaultdict(list) # {userId: [(time, tweetId), ..]}
+        self.time = 0 # tweet  time += 1
+        
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweets[userId].append((self.time, tweetId))
         self.time += 1
+        
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        heap = []
+        # add foloweeId + userId into one list
+        users = list(self.following[userId]) + [userId]
         users = self.following[userId] | {userId}
-        for uid in users:
-            if self.tweets[uid]:
-                idx = len(self.tweets[uid]) - 1
-                t, tid = self.tweets[uid][idx]
-                heapq.heappush(heap, (-t, tid, uid, idx))
+        
+        # pq = []
+        # for user in users:
+        #     for time, tweet in self.tweets[user][-10:]:
+        #         heapq.heappush(pq, (time, tweet))
+        #         if len(pq) > 10:
+        #             heapq.heappop(pq)
+          #O(10*nlog10)          
+        # res = []
+        # while pq:
+        #     _, tweet = heapq.heappop(pq)
+        #     res.append(tweet)
+        #O(10log10)
+        # return res[::-1]
+
+        heap = []
+        for user in users:
+            if self.tweets[user]:
+                idx = len(self.tweets[user]) - 1
+                time, t_id = self.tweets[user][idx]
+                heapq.heappush(heap, (-time, t_id, user, idx))
+        #O(nlogn)
         res = []
         while heap and len(res) < 10:
-            neg_t, tid, uid, idx = heapq.heappop(heap)
-            res.append(tid)
+            t, t_id, user, idx = heapq.heappop(heap)
+            res.append(t_id)
             if idx > 0:
                 idx -= 1
-                t, tid = self.tweets[uid][idx]
-                heapq.heappush(heap, (-t, tid, uid, idx))
+                time, tweetId = self.tweets[user][idx]
+                heapq.heappush(heap, (-time, tweetId, user, idx))
         return res
+        #O(10logn)  
+        #500
+                 
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.following[followerId].add(followeeId)
+        if followerId != followeeId:
+            self.following[followerId].add(followeeId)
+        
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         self.following[followerId].discard(followeeId)
+        
 
 
 # Your Twitter object will be instantiated and called as such:
