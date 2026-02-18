249b2c45e37af3bece065b0ae3ab2f88c6f02998
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-18 15:54:12 -0800

    Time: 5736 ms (40.84%), Space: 21 MB (50.24%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,50 +1,41 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.word = None #直接存单词，省去回溯拼接
+        self.children = {} # char -> node
+        self.word = None
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        root = TrieNode()
-        
-        for s in words:
-            node = root
-            for c in s:
-                if not c in node.children:
-                    node.children[c] = TrieNode()
-                node = node.children[c]
-            node.word = s
-
-        rows, cols = len(board), len(board[0])   
         res = []
-
+        m, n = len(board), len(board[0])
+        #put all the words into the Trie
+        root = TrieNode()
+        for word in words:
+            node = root
+            for ch in word:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            node.word = word
+        
+        #dfs search from 4 dir of each cell
+        dir = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        
         def dfs(r, c, node):
-            ch = board[r][c]
-
-            if ch not in node.children:
-                return
-            
-            dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-            next_node = node.children[ch]
-
-            if next_node.word:
-                res.append(next_node.word)
-                next_node.word = None
-            
-            board[r][c] = "#"
-
-            for a, b in dir:
-                a1, b1 = r + a, c + b
-                if 0 <= a1 < rows and 0 <= b1 < cols and board[a1][b1] != "#":
-                    dfs(a1, b1, next_node)
-
-            board[r][c] = ch
-            if not next_node.children:
-                del node.children[ch]
-            
-        for r in range(rows):
-            for c in range(cols):
-                dfs(r, c, root)
-
+            if node.word:
+                res.append(node.word)
+                node.word = None
+            if 0 <= r < m and 0 <= c < n and board[r][c] != "#":    
+                ch = board[r][c] #a
+                if ch in node.children:
+                    board[r][c] = "#"
+                    for a, b in dir:
+                        dfs(r + a, c + b, node.children[ch])
+                    board[r][c] = ch
+            return
+        
+        #iterate through all the cells in the grid and do the dfs
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+        
         return res
