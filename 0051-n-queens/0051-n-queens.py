f416536f58bc916e6f8d273195bee94d916b0bf7
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-19 16:38:58 -0800

    Time: 10 ms (75.52%), Space: 19.7 MB (52.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,33 +1,41 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set() # store which col has queen
+        diag1 = set() # row - col
+        diag2 = set() # row + col
+        queens = [-1] * n #queens[row] = col 
         res = []
-
-        cols = set() #record which col has the queen
-        diag1 = set() #diag row - col
-        diag2 = set() #diag row + col
-        queens = [-1] * n #queens[row] = col record which col queens were put in each row
-
+        
         def backtrack(row):
             if row == n:
-                board = []
+                temp = []
                 for r in range(n):
-                    board.append("." * queens[r] + "Q" + "." * (n - queens[r] - 1))
-                res.append(board)
+                    temp.append('.' * queens[r] + 'Q' + '.' * (n - 1 - queens[r]))
+                res.append(temp)
                 return
-
             for col in range(n):
+                #pruning
                 if col in cols or (row - col) in diag1 or (row + col) in diag2:
                     continue
+                #backtracking
+                
                 queens[row] = col
+                
                 cols.add(col)
                 diag1.add(row - col)
                 diag2.add(row + col)
-
+                
                 backtrack(row + 1)
-
+                
                 cols.remove(col)
                 diag1.remove(row - col)
                 diag2.remove(row + col)
-
+                
         backtrack(0)
         return res
+                
+            
+                
+                
+        
+        
