eb9eb451b552a7675655cb03d096362228d9aaad
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-20 11:14:41 -0800

    Time: 35 ms (96.63%), Space: 20.6 MB (65.37%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -6,21 +6,26 @@ class Solution:
         wordSet = set(wordList)
         if endWord not in wordSet:
             return 0
-
-        queue = deque([(beginWord, 1)])
-        visited = set()
-        visited.add(beginWord)
-
-        while queue:
-            s, cnt = queue.popleft()
-            for i in range(len(s)):
-                for c in string.ascii_lowercase:
-                    new_word = s[:i] + c + s[i+1:]
-                    if new_word == endWord:
-                        return cnt + 1
-                    if new_word in wordSet and new_word not in visited:
-                        queue.append((new_word, cnt + 1))
-                        visited.add(new_word)
+        
+        front = {beginWord}
+        back = {endWord}
+        visited = {beginWord, endWord}
+        length = 1
+        while front and back:
+            length += 1
+            #每次选择较小的方向拓展
+            if len(front) > len(back):
+                front, back = back, front
+            #拓展front
+            next_front = set()
+            for word in front:
+                for i in range(len(word)):
+                    for c in string.ascii_lowercase:
+                        new_w = word[:i] + c + word[i+1:]
+                        if new_w in back:
+                            return length
+                        if new_w in wordSet and new_w not in visited:
+                            next_front.add(new_w)
+                            visited.add(new_w)
+            front = next_front
         return 0
-
-            
