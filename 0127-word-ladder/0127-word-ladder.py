2b75e991267275ffb7eb8dc91e2e8a35f768c29d
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-23 15:04:10 -0800

    Time: 37 ms (95.03%), Space: 20.6 MB (67.22%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,31 +1,30 @@
-from collections import deque
 import string
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordSet = set(wordList)
-        if endWord not in wordSet:
+        if not endWord in wordSet:
             return 0
+        
         
         front = {beginWord}
         back = {endWord}
         visited = {beginWord, endWord}
-        length = 1
+        res = 1
+        
         while front and back:
-            length += 1
-            #每次选择较小的方向拓展
+            res += 1
             if len(front) > len(back):
                 front, back = back, front
-            #拓展front
             next_front = set()
             for word in front:
                 for i in range(len(word)):
                     for c in string.ascii_lowercase:
-                        new_w = word[:i] + c + word[i+1:]
-                        if new_w in back:
-                            return length
-                        if new_w in wordSet and new_w not in visited:
-                            next_front.add(new_w)
-                            visited.add(new_w)
+                        new_word = word[:i] + c + word[i+1:]
+                        if new_word in back:
+                            return res
+                        if new_word in wordSet and new_word not in visited:
+                            next_front.add(new_word)
+                            visited.add(new_word)
             front = next_front
+        
         return 0
