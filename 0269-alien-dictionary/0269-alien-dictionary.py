9e3ed8ce5cd8abc72ed1077b680e49603041955b
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-26 11:34:16 -0800

    Time: 2 ms (45.84%), Space: 19.6 MB (32.5%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,39 +1,31 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        #adjacency list
-        graph = {c : set() for word in words for c in word}
-
-        #get word order
+        # model graph
+        graph = defaultdict(set)
+        indegree = {c: 0 for word in words for c in word}
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
             min_len = min(len(w1), len(w2))
-            
+            #非法
             if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
                 return ""
-
             for j in range(min_len):
                 if w1[j] != w2[j]:
-                    graph[w1[j]].add(w2[j])
+                    if w2[j] not in graph[w1[j]]:
+                        graph[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
                     break
-
-        #calculate indegree   
-        indegree = {c: 0 for c in graph}
-        for c in graph:
-            for neighbor in graph[c]:
-                indegree[neighbor] += 1
-        #bfs/ kahn's algorithm
-        queue = deque([c for c in indegree if indegree[c] == 0])
+            
+        queue = deque(c for c in indegree if indegree[c] == 0)
         res = []
-
         while queue:
-            c = queue.popleft()
-            res.append(c)
-            for neighbor in graph[c]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-
-        if len(res) != len(graph):
+            node = queue.popleft()
+            res.append(node)
+            for course in graph[node]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+        if len(res) != len(indegree):
             return ""
-        
         return "".join(res)
+        
