886ab1195c743a8976f3465a3a1eadbec0b8bfc1
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-02-28 12:02:32 -0800

    Time: 0 ms (100%), Space: 19.4 MB (37.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        phone_map = {
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
-        }
+        hashmap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         res = []
 
-        def backtrack(idx, path):
+        def backtracking(idx, path):
             if idx == len(digits):
-                res.append(''.join(path[:]))
-                return 
-            
-            for ch in phone_map[digits[idx]]:
-                path.append(ch)
-                backtrack(idx + 1, path)
-                path.pop()
-        
-        backtrack(0, [])
-        return res
+                res.append("".join(path))
+                return
 
+            for c in hashmap[digits[idx]]:
+                path.append(c)
+                backtracking(idx + 1, path)
+                path.pop()
+
+        backtracking(0, [])
+        return res
+                
+                
+        
