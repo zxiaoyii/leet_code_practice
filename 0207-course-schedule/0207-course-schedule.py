9bce47ed97a790a5ca02f38d6aa2c5c68a5d1a05
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-03-03 09:26:07 -0800

    Time: 3 ms (87.52%), Space: 20.4 MB (87.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,21 +1,27 @@
-from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list) #course -> []
+        graph = defaultdict(list) # course -> []
         indegree = [0] * numCourses
         for a, b in prerequisites:
             graph[b].append(a)
             indegree[a] += 1
         
-        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
-        count = 0 # how may course finished
+        # bfs(dijkstra)
+
+        queue = deque([i for i in range(len(indegree)) if indegree[i] == 0])
+
         while queue:
-            course = queue.popleft()
-            count += 1
-            for next_c in graph[course]:
-                indegree[next_c] -= 1
-                if indegree[next_c] == 0:
-                    queue.append(next_c)
-        return count == numCourses
+            idx = queue.popleft()
+            for course in graph[idx]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+        
+        for i in indegree:
+            if i != 0:
+                return False
+        return True
+                    
 
 
+        
