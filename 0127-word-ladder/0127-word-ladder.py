431cb73d8308782a61c57a2cfef38c1313e033a7
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-03-03 09:47:01 -0800

    Time: 297 ms (40.2%), Space: 20.5 MB (80.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,23 +1,21 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordSet = set(wordList)
-        if endWord not in wordList:
+        if endWord not in wordSet:
             return 0
-        
+        # bfs
         queue = deque([(1, beginWord)])
-        visited = {beginWord}
+        visited = set()
+        visited.add(beginWord)
         while queue:
             step, word = queue.popleft()
             for i in range(len(word)):
                 for c in ascii_lowercase:
-                    newWord = word[:i] + c + word[i+1:]
-                    if newWord == endWord:
+                    new_word = word[:i] + c + word[i+1:]
+                    if new_word == endWord:
                         return step + 1
-                    if newWord in wordSet and newWord not in visited:
-                        visited.add(newWord)
-                        queue.append((step + 1, newWord))
+                    if new_word in wordSet and new_word not in visited:
+                        visited.add(new_word)
+                        queue.append((step + 1, new_word))
         return 0
-        
-                
-                
-                
+                        
