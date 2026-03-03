71b9a0acd928bd6e660298a586e25cebc245e8a4
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-03-03 12:55:40 -0800

    Time: 3 ms (47.43%), Space: 19.6 MB (73.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,24 +3,25 @@ class Solution:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
         m, n = len(nums1), len(nums2)
-        half_length = (m + n + 1) // 2
+        length = (m + n + 1) // 2
         l, r = 0, m
-
         while l <= r:
-            i = (l + r) // 2
-            j = half_length - i
-            nums1_a = nums1[i - 1] if i > 0 else float('-inf')
-            nums1_b = nums1[i] if i < m else float('inf')
-            nums2_a = nums2[j - 1] if j > 0 else float('-inf')
-            nums2_b = nums2[j] if j < n else float('inf')
-            if nums1_a <= nums2_b and nums1_b >= nums2_a:
-                if (m + n) % 2 == 1:
-                    return float(max(nums1_a, nums2_a))
-                else:
-                    return (max(nums1_a, nums2_a) + min(nums1_b, nums2_b)) / 2
-            elif nums1_a > nums2_b:
-                r = i - 1 
-            else:
+            i = (l + r) //2
+            j = length - i
+            a = nums1[i - 1] if i > 0 else float('-inf')
+            b = nums1[i] if i < m else float('inf')
+            c = nums2[j - 1] if j > 0 else float('-inf')
+            d = nums2[j] if j < n else float('inf')
+            if a > d:
+                r = i - 1
+            elif b < c:
                 l = i + 1
-                 
+            elif a <= d and b >= c:
+                if (m + n) % 2 == 1:
+                    return float(max(a, c))
+                else:
+                    return (max(a, c) + min(b, d)) / 2
 
+        
+        
+        
