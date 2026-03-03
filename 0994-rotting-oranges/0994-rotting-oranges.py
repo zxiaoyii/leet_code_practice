63dc2e541ac312927e53e0afb448065737bbee78
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-03-03 09:58:26 -0800

    Time: 7 ms (17.8%), Space: 19.4 MB (73.95%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,43 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        queue = deque()
+        rotten = 0
         fresh = 0
-        
-        #count the num of fresh and rotten oranges
+        queue = deque([])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0,-1)]
+        res = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                elif grid[i][j] == 1:
+                if grid[i][j] == 1:
                     fresh += 1
-        
+                elif grid[i][j] == 2:
+                    rotten += 1
+                    queue.append((i, j))
         if fresh == 0:
             return 0
-        
-        mins = 0
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        while queue and fresh > 0:
-            mins += 1
+        if rotten == 0:
+            return -1
+
+        # bfs
+        while queue:
+            res += 1
             for _ in range(len(queue)):
-                r, c = queue.popleft()
+                i, j = queue.popleft()
                 for dr, dc in dirs:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
+                    nr, nd = i + dr, j + dc
+                    if 0 <= nr < m and 0 <= nd < n and grid[nr][nd] == 1:
+                        grid[nr][nd] = 2
+                        queue.append((nr, nd))
                         fresh -= 1
-                        queue.append((nr, nc))
-        return mins if fresh == 0 else -1
-            
+                
+        if fresh > 0:
+            return -1
+        else:
+            return res - 1
+                        
+
+        
+
+        
+        
+                
