a1a1521a05d5c8853445c373ef4a866629079ea0
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-12 22:12:53 +0800

    Time: 0 ms (100%), Space: 19.7 MB (32.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-
+        
         def backtrack(path, start, sum):
             if sum == target:
                 res.append(path[:])
@@ -10,10 +10,8 @@ class Solution:
                 if candidates[i] > target - sum:
                     break
                 path.append(candidates[i])
-                backtrack(path, i ,sum + candidates[i]) #可重复所以是i
+                backtrack(path, i, sum + candidates[i])
                 path.pop()
-
-
         candidates.sort()
         backtrack([], 0, 0)
         return res
