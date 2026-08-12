31ab805232007c06cf14cccccf0effa9bc10ecdb
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-12 17:52:07 +0800

    Time: 0 ms (100%), Space: 27.7 MB (97.04%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,43 +8,32 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        # Only have one node
         if not head.next:
             return
-        
-        #split the linked list from the middle
-            #find the middle node of the linked list
-            
-        fast, slow = head, head
-        while fast.next and fast.next.next:
+        # Split from middle
+        fast = head
+        slow = head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        #spilt the linked list
-        #reverse the right part of the split linked list
+
+        # Reverse the right part
         prev = None
         cur = slow.next
-        slow.next = None    
+        slow.next = None
         while cur:
             node = cur.next
             cur.next = prev
             prev = cur
             cur = node
-        #[12]|[43]   
-            
-        #merge the two linked list together
+        # Merge two list together
         while head and prev:
-            temp1 = head.next
-            temp2 = prev.next
+            node = head.next
             head.next = prev
-            prev.next = temp1
-            head = temp1
-            prev = temp2
-        
-            
-        
-        
-        
-        
-        
-        
-        
-        
+            node1 = prev.next
+            prev.next = node
+            head = node
+            prev = node1
+
+          
