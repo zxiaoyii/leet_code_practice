987dc763219a639f7ba10456c3691a7b06cd63b3
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-12 23:36:39 +0800

    Time: 4138 ms (40.36%), Space: 19.3 MB (83.37%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-
+        m = len(board)
+        n = len(board[0])
+        dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
         def dfs(i, j, idx):
             if idx == len(word):
                 return True
-            for dr, dc in dirs:
-                nr, nc = dr + i, dc + j
-                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == word[idx]:
-                    c = board[nr][nc]
-                    board[nr][nc] = '#'
-                    if dfs(nr, nc, idx + 1):
-                        return True
-                    board[nr][nc] = c
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[idx]:
+                return False
+            if idx == len(word) - 1:
+                return True
+            c = board[i][j]
+            board[i][j] = "#"
 
+            for dx, dy in dir:
+                x = i + dx
+                y = j + dy
+                if dfs(x, y, idx + 1):
+                    return True
+            board[i][j] = c    
+        
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0]:
-                    board[i][j] = '#'
-                    if dfs(i, j, 1):
-                        return True
-                    board[i][j] = word[0]
+                if dfs(i, j, 0):
+                    return True
         return False
 
-        
-            
+                    
