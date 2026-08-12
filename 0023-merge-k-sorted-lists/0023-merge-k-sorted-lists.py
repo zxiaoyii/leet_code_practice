a15d70f1de3b085b546ba3b4cce31058363a59a1
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-12 18:32:00 +0800

    Time: 7 ms (83.08%), Space: 22.9 MB (59.44%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,23 +7,20 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-            
-        heap = []
-        # traverse all the list in lists and add the first node into heap
-        for i, node in enumerate(lists):
-            if node:
-                heapq.heappush(heap, (node.val, i, node))
+        h = []
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(h, (l.val, i, l))
         
         dummy = ListNode(0)
         cur = dummy
-        # while heap is not empty
-            # pop temp_node and add to the result linked list
-            # add the node next to temp_node to the heap
-        while heap:
-            temp_val, temp_i, temp_node = heapq.heappop(heap) 
-            cur.next = temp_node
-            cur = cur.next  
-            if temp_node.next:
-                heapq.heappush(heap, (temp_node.next.val, temp_i, temp_node.next)) 
-            
+        while h:
+            val, i, l = heapq.heappop(h)
+            cur.next = l
+            cur = cur.next
+            if l.next:
+                node =l.next
+                l.next = None
+                heapq.heappush(h, (node.val, i, node))
         return dummy.next
+
