8a267efed462206daabc16547f14be74d58a85da
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-14 10:52:14 +0800

    Time: 1 ms (43.03%), Space: 21.4 MB (56.83%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,12 +4,9 @@ class Solution:
         i = 0
         n = len(intervals)
 
-        #add all intervals at the left side of the new one into res
         while i < n and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
             i += 1
-        
-        #merge all the overlapping intervals
         while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
@@ -19,5 +16,7 @@ class Solution:
         while i < n:
             res.append(intervals[i])
             i += 1
-
         return res
+
+
+            
