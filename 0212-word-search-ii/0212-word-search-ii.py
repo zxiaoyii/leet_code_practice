e02b283909111ce5c5c1edd66cdb6745852acea0
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-14 16:30:58 +0800

    Time: 5686 ms (36.92%), Space: 20.9 MB (79.85%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,9 +1,10 @@
 class TrieNode:
     def __init__(self):
-        self.children = {} # char -> node
+        self.children = {}
         self.word = None
-
+        
 class Solution:
+
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         res = []
         m, n = len(board), len(board[0])
@@ -39,3 +40,4 @@ class Solution:
                 dfs(i, j, root)
         
         return res
+                
