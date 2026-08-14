14fd446d5f5514401a69abd19c5bd251e4c67458
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-14 12:51:00 +0800

    Time: 3 ms (84.88%), Space: 21.4 MB (53.71%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,21 +1,16 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        #sort the intervals by its start time
         intervals.sort()
-        #minheap to put all the end time
         heap = []
         res = 0
-        #traverse the intervals 
-            #if the start time is bigger or = than the heap[0]
-                #use the conference room
-            #else 
-                #add room by one
         for s, e in intervals:
             if heap and s >= heap[0]:
                 heapq.heapreplace(heap, e)
             else:
                 heapq.heappush(heap, e)
                 res += 1
-        #return the room num
         return res
+                
+
+
         
