78a4afe1d2c57b586f012f73ba9678a8c2939155
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-15 11:32:16 +0800

    Time: 35 ms (99.34%), Space: 19.7 MB (54.53%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,17 +11,15 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        visited = {} #old node -> new node
+        visited = {}
 
         def dfs(node):
             if node in visited:
                 return visited[node]
-
             clone = Node(node.val)
             visited[node] = clone
-
             for neighbor in node.neighbors:
                 clone.neighbors.append(dfs(neighbor))
             return clone
-            
-        return dfs(node)        
+
+        return dfs(node)
