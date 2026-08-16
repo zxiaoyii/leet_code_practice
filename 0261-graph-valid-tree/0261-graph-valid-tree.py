4f8a6dbcad8c2a444c5d50bc3f56290c957ab371
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-16 09:14:48 +0800

    Time: 0 ms (100%), Space: 20.3 MB (90.15%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,12 +1,9 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        # 1. don't have circle
-        # 2. n nodes, n - 1 edges
-
         if len(edges) != n - 1:
             return False
         
-        parent = list(range(n)) 
+        parent = list(range(n))
         rank = [0] * n
 
         def find(x):
@@ -16,21 +13,19 @@ class Solution:
 
         def union(x, y):
             px, py = find(x), find(y)
-            if px == py: #have the same root , and if add this edge, will be circle
-                return False 
+            if px == py:
+                return False
             if rank[px] < rank[py]:
                 px, py = py, px
-
             parent[py] = px
             if rank[px] == rank[py]:
                 rank[px] += 1
             return True
-        
+
         for a, b in edges:
             if not union(a, b):
                 return False
         return True
-
 
 
         
