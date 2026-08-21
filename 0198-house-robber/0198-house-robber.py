57ca44276e7a27c0eae4921674c535cfb0fdbfa1
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-21 20:59:12 +0800

    Time: 0 ms (100%), Space: 19.2 MB (62.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev2, prev1 = 0, 0
-        
-        for num in nums:
-            curr = max(prev1, prev2 + num)
-            prev2, prev1 = prev1, curr
-        
-        return prev1
+        p1 = 0
+        p2 = 0
 
+        res = 0
+        for i, n in enumerate(nums):
+            res = max(p1 + n, p2) 
+            p1 = p2 
+            p2 = res 
+        return res
+        
 
