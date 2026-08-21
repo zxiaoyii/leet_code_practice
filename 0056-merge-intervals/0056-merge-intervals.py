3f8f249c639213e811c1c541d9237e387ad1e844
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-08-22 00:36:13 +0800

    Time: 11 ms (24.72%), Space: 23.3 MB (7.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,10 +4,8 @@ class Solution:
         res = []
         res.append(intervals[0])
         for s, e in intervals[1:]:
-            s1 = res[-1][0]
             e1 = res[-1][1]
             if s <= e1:
-                res[-1][0] = min(s1, s)
                 res[-1][1] = max(e1, e)
             else:
                 res.append([s, e])
