02028e3bb7497775b026df7741142729a1fb3b9a
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-16 03:42:29 +0800

    Time: 93 ms (58.99%), Space: 28.4 MB (60.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
         n = len(temperatures)
-        answer = [0] * n      # 初始化全0
-        stack = []            # 单调栈，存索引
-        
-        for i in range(n):
-            # 当前温度 > 栈顶温度，找到答案了
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                prev_index = stack.pop()
-                answer[prev_index] = i - prev_index
-            
-            stack.append(i)   # 当前索引入栈
-        
-        return answer
-
-            
-            
+        ans = [0] * n
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+        return ans
