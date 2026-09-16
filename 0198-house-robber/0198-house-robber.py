4c730c086f7fcc1f8499b06b252bbff56e09b181
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-17 02:55:33 +0800

    Time: 0 ms (100%), Space: 19.4 MB (25.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,11 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        p1 = 0
-        p2 = 0
-
+    def rob(self, nums: list[int]) -> int:
+        #dp[n + 2] = max(dp[n] + num[n + 2], dp[n + 1])
+        a = 0
+        b = 0
         res = 0
-        for i, n in enumerate(nums):
-            res = max(p1 + n, p2) 
-            p1 = p2 
-            p2 = res 
+        for num in nums:
+            res = max(a + num, b)
+            a = b
+            b = res
         return res
-        
-
