1126e04052ef7f6957a7a7a50395d9757b40b221
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-17 01:29:04 +0800

    Time: 29 ms (80.19%), Space: 34.2 MB (36.08%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,36 +1,41 @@
 class TrieNode:
-
     def __init__(self):
-        self.children = {}
         self.is_end = False
+        self.children = {}
 
 class Trie:
 
     def __init__(self):
         self.root = TrieNode()
-       
+
     def insert(self, word: str) -> None:
         node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = TrieNode()
-            node = node.children[ch]
+        for c in word:
+            if not c in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
         node.is_end = True
-
+   
     def search(self, word: str) -> bool:
-        node = self._find(word)
-        return node is not None and node.is_end
+        a = self._exist(word)
+        if a:
+            return a
+        return False 
+        
 
     def startsWith(self, prefix: str) -> bool:
-        return self._find(prefix) is not None
-
-    def _find(self, s: str):
+        return self._exist(prefix) is not None
+    
+    def _exist(self, w: str) -> bool:
         node = self.root
-        for ch in s:
-            if ch not in node.children:
+        for c in w:
+            if c in node.children:
+                node = node.children[c]
+            else:
                 return None
-            node = node.children[ch]  
-        return node  
+        return node.is_end
+        
+        
 
 
 # Your Trie object will be instantiated and called as such:
