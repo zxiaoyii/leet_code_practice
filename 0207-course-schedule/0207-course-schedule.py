b9976e6bf0f5d3a94908836ba7f5806f6ad00029
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-17 01:39:52 +0800

    Time: 4 ms (64.19%), Space: 20.4 MB (62.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,22 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # model the graph
-        # course -> [list of course can be taken after took this course]
-        graph = defaultdict(list) 
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        #model graph
+        graph = defaultdict(list)
         indegree = [0] * numCourses
         for a, b in prerequisites:
             graph[b].append(a)
             indegree[a] += 1
-
-        # dijkstra
         queue = deque([i for i in range(len(indegree)) if indegree[i] == 0])
 
         while queue:
-            idx = queue.popleft()
-            for course in graph[idx]:
+            c = queue.popleft()
+            for course in graph[c]:
                 indegree[course] -= 1
                 if indegree[course] == 0:
                     queue.append(course)
-
         for i in indegree:
             if i != 0:
                 return False
         return True
-        
+
+
