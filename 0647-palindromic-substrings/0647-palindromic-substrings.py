2d09abade88f4b328ab3366f4605e6fd26abd2cf
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-18 05:42:17 +0800

    Time: 103 ms (93.55%), Space: 19.5 MB (22.26%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,20 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        self.res = 0
-        def check(i, j):
-            if 0 <= i and j < n and s[i] == s[j]:
-                self.res += 1
-                check(i - 1, j + 1)
-            return
-
         res = 0
-        for i in range(n):
-            check(i, i)
-            check(i, i + 1)
-            
-        return self.res
-            
 
+        def expand(i, j):
+            cnt = 0
+            while i >= 0 and j < n and s[i] == s[j]:
+                cnt += 1
+                i -= 1
+                j += 1
+            return cnt
+
+        for i in range(n):
+            res += expand(i, i)      # 奇数长度，单字符为中心
+            res += expand(i, i + 1)  # 偶数长度，两字符之间为中心
+        return res
 
        
