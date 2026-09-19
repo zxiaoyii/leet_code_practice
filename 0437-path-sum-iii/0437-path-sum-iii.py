07730d3b4032703e6c4e46b6ff9e424ec1759e8c
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-19 13:42:51 +0800

    Time: 3 ms (89.43%), Space: 20.7 MB (27.65%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -5,20 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix = defaultdict(int) # sum -> count
+    def pathSum(self, root: TreeNode | None, targetSum: int) -> int:
+        prefix = defaultdict(int)
         prefix[0] = 1
 
-        def dfs(node, s):
+        def dfs(node, cur):
             if not node:
                 return 0
+            cur += node.val
+            res = prefix[cur - targetSum]
 
-            new_s = node.val + s
-            count = prefix[new_s - targetSum]
-            prefix[new_s] += 1
-            count += dfs(node.left, new_s) + dfs(node.right, new_s) 
-            prefix[new_s] -= 1
-            return count
-        
+            prefix[cur] += 1
+            res += dfs(node.left, cur)
+            res += dfs(node.right, cur)
+            prefix[cur] -= 1
+            return res
         return dfs(root, 0)
-            
