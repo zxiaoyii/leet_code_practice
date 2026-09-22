2f76198859a6166b286da490198e4a0adba57274
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-22 09:43:30 +0800

    Time: 0 ms (100%), Space: 21.1 MB (98.8%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,10 +3,11 @@ class Solution:
         res = []
         i = 0
         n = len(intervals)
-
+        
         while i < n and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
             i += 1
+        
         while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
@@ -20,3 +21,4 @@ class Solution:
 
 
             
+                
