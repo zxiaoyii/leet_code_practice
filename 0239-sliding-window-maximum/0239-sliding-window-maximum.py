c0c8f772cc018d6da14276757daef838afe391ef
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-22 20:34:32 -0700

    Time: 140 ms (99.26%), Space: 34.9 MB (71.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,22 +1,14 @@
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        l = 0
-        window = deque() 
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        q = deque()
         res = []
         for r in range(len(nums)):
-            cur = nums[r]
-            #remove the numbers smaller than the cur number from the right side
-            while window and nums[window[-1]] < cur:
-                window.pop()
-            window.append(r)
-            
-            #popleft all the elements that are not in the sliding window
-            while window[0] < r - k + 1:
-                window.popleft()
-            #append the max num in the window to the res list 
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if q[0] <= r - k:
+                q.popleft()
             if r >= k - 1:
-                res.append(nums[window[0]])
+                res.append(nums[q[0]])
         return res
-                 
-            
-        
