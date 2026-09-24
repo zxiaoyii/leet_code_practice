9f8008d55d8dd6afe7e9e8ef05602ea93a708133
zxiaoyii <118496833+zxiaoyii@users.noreply.github.com>
2026-09-24 13:22:30 -0700

    Time: 0 ms (100%), Space: 19.4 MB (79.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,23 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
         m, n = len(nums1), len(nums2)
-        length = (m + n + 1) // 2
+        mid = (m + n + 1) // 2
         l, r = 0, m
         while l <= r:
-            i = (l + r) //2
-            j = length - i
+            i = (l + r) // 2
+            j = mid - i
             a = nums1[i - 1] if i > 0 else float('-inf')
             b = nums1[i] if i < m else float('inf')
             c = nums2[j - 1] if j > 0 else float('-inf')
             d = nums2[j] if j < n else float('inf')
-            if a > d:
-                r = i - 1
-            elif b < c:
-                l = i + 1
-            elif a <= d and b >= c:
-                if (m + n) % 2 == 1:
-                    return float(max(a, c))
-                else:
-                    return (max(a, c) + min(b, d)) / 2
 
-        
-        
-        
+            if a <= d and c <= b:
+                if (m + n) % 2:
+                    return max(a, c)
+                return (max(a, c) + min(b, d)) / 2
+            elif a > d:
+                r = i - 1
+            else:
+                l = i + 1
